64c691ebbef2cc1ecfce9cc453ce1267f47ad3e6
Juergen Felleisen <juergen.felleisen@googlemail.com>
2010-12-22 21:15:17 +0100

    initial commit

---
+++ sms2email/src/de/felleisen/android/sms2email/Sms2Email.java
@@ -0,0 +1,13 @@
+package de.felleisen.android.sms2email;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Sms2Email extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
